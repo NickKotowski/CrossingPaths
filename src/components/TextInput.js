2ce163ca6467a2a2c182c645988b98c3ae5eaf5b
Quynh Nguyen <vanquynh6894@gmail.com>
2019-01-05 14:00:13 +0700

    Finish button, test TextInput

---
+++ src/components/TextInput.js
@@ -0,0 +1,35 @@
+import React, { Component, Fragment } from 'react';
+import { TextInput, Text, View, StyleSheet } from'react-native';
+import styles from './styles';
+
+export default class Button extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        const { label, labelStyle, inputStyle, ...more } = this.props;
+        return(
+            <Fragment>
+                {label && <Text style={[styles.txtLabel, labelStyle]}>
+                    {label}
+                </Text>}
+                <TextInput
+                    style={styles.input, inputStyle}
+                    {...more}
+                />
+            </Fragment>
+        );
+    }
+}
+
+const style = StyleSheet.create({
+    txtLabel: {
+        fontSize: 18,
+        fontStyle: 'italic',
+    },
+
+    input:{
+        fontStyle: 'italic',
+    }
+})
