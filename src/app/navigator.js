dd1e798c9d01b1ceb5dab2fa120cf066e4fcb0cf
Andrea <andrea.belluccia@gmail.com>
2019-01-05 16:20:41 +0700

    Create login screen

--- src/app/navigator.js
+++ src/app/navigator.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { createBottomTabNavigator, createAppContainer } from "react-navigation";
 
+import Login from '../screens/Login';
 import Home from '../screens/Home';
 import CounterNavigator from './counterNavigator';
 
 const AppNavigator = createBottomTabNavigator({
+  Login: {
+    screen: Login,
+    title: 'Login',
+  },
   Home: {
     screen: Home,
     title: 'Home',
